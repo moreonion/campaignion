3d5d731d77f5707e2bdddfeb630cfb691b755931
Roman Zimmermann <roman@more-onion.com>
2013-08-12 10:42:16 +0200

    Remove console.log call. #2664

--- modules/webform_steps/webform_steps.js
+++ modules/webform_steps/webform_steps.js
@@ -2,7 +2,6 @@
 
 Drupal.behaviors.webform_steps = {};
 Drupal.behaviors.webform_steps.attach = function(context, settings) {
-  console.log($('.webform-steps-wrapper', context));
   $('.webform-steps-wrapper', context).click(function(event) {
     var $target = $(event.target);
     if ($target.is('span')) {
