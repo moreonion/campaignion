61ed6da041b06b93438625deaf9a39638b348792
Roman Zimmermann <roman@more-onion.com>
2013-11-08 12:24:00 +0100

    Refactor listing and query into classes. #3093

---
+++ campaignion_manage/lib/ContentListing.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Drupal\campaignion_manage;
+
+class ContentListing {
+  /**
+   * Build a renderable array based on the data-rows.
+   *
+   * @param rows result from the Query object
+   * @return renderable array for output.
+   */
+  public function build($result) {
+    $row_default['data']['bulk']['data'] = array(
+      '#type' => 'checkbox',
+    );
+    $row_default['data']['content']['data'] = array();
+    $row_default['data']['links']['data'] = array(
+      '#theme' => 'links__ctools_dropbutton',
+      '#links' => array(),
+      '#image' => TRUE,
+    );
+    $columns = count($row_default['data']);
+
+    $rows = array();
+
+    $tnode_count = 1;
+    foreach ($result as $tnode) {
+      $class = ($tnode_count++ % 2 == 0) ? 'even' : 'odd';
+      $row = $row_default;
+      $row['no_striping'] = TRUE;
+      $row['data']['links']['data']['#links'] = $this->nodeLinks($tnode);
+      $row['data']['bulk']['data'] += array(
+        '#name' => 'bulk_tnid[]',
+        '#attributes' => array('title' => "Select this content and all it's translations for bulk operations."),
+        '#value' => $tnode->nid,
+      );
+      $row['data']['content']['data'] = array(
+        '#markup' => 'Translation set: ' . $tnode->title,
+      );
+      $row['class'][] = $class;
+      if (count($tnode->translations) > 1) {
+        $row['class'][] = 'node-translation-set';
+        $rows[] = $row;
+
+        $bigcellrow['data']['bigcell']['colspan'] = $columns;
+        $bigcellrow['class'][] = $class;
+        $innerRows = array();
+        foreach ($tnode->translations as $lang => $node) {
+          $row = $row_default;
+          $row['data']['bulk']['data'] += array(
+            '#name' => 'bulk_nid[]',
+            '#attributes' => array('title' => "Select this content for bulk operations."),
+            '#value' => $node->nid,
+          );
+          $row['data']['content']['data'] = array(
+            '#markup' => 'Translation: ' . $node->title,
+          );
+          $row['data']['links']['data']['#links'] = $this->nodeLinks($node);
+          $innerrows[] = $row;
+        }
+        $bigcellrow['data']['bigcell']['data'] = array(
+          '#theme' => 'table',
+          '#rows' => $innerrows,
+        );
+        $rows[] = $bigcellrow;
+      } else {
+        $rows[] = $row;
+      }
+    }
+
+    return array(
+      '#theme' => 'table',
+      '#attributes' => array(
+        'class' => array('campaignion-manage-content-listing'),
+      ),
+      '#rows' => $rows,
+    );
+  }
+
+  protected function nodeLinks($node) {
+    $links = array();
+    foreach (array('edit' => t('Edit'), 'translate' => t('Translate'), 'view' => t('View page'), 'delete' => t('Delete')) as $path => $title) {
+      $links[$path] = array(
+        'href' => "node/{$node->nid}/$path",
+        'title' => $title,
+      );
+    }
+    return $links;
+  }
+}
---
+++ campaignion_manage/lib/ContentQuery.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\campaignion_manage;
+
+class ContentQuery {
+  protected $query;
+
+  public function __construct() {
+    $this->build();
+  }
+
+  public function build() {
+    $query = db_select('node', 'n');
+    $query->innerJoin('users', 'u', 'u.uid = n.uid');
+    $query->fields('n', array('nid', 'title', 'type', 'language', 'status', 'uid'))
+      ->fields('u', array('name'))
+      ->where('n.nid = n.tnid OR n.tnid = 0')
+      ->orderBy('n.changed', 'DESC');
+    $this->query = $query;
+  }
+
+  public function execute() {
+    $rows = $this->query->execute()->fetchAll();
+    $this->modifyResult($rows);
+    return $rows;
+  }
+
+  public function page($size) {
+    $this->query = $this->query->extend('PagerDefault')->limit($size);
+  }
+
+  public function modifyResult(&$rows) {
+    if (empty($rows)) {
+      return;
+    }
+    $rows_by_nid = array();
+    foreach ($rows as $row) {
+      $row->translations = array();
+      $rows_by_nid[$row->nid] = $row;
+    }
+
+    $sql = <<<SQL
+SELECT n.tnid, n.nid, n.title, n.language, n.status, n.uid, u.name
+FROM {node} n 
+  INNER JOIN {users} u ON u.uid=n.uid
+WHERE n.tnid IN(:nids)
+ORDER BY n.language
+SQL;
+    $result = db_query($sql, array(':nids' => array_keys($rows_by_nid)));
+    foreach ($result as $row) {
+      $rows_by_nid[$row->tnid]->translations[$row->language] = $row;
+    }
+  }
+}
