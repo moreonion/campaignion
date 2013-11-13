6724517d89110900003adf97085629a5f700e8de
Webdev <webdev@more-onion.com>
2013-11-13 22:16:19 +0100

    Log not yet logged submissions when confirming.

--- campaignion_activity/modules/webform.php
+++ campaignion_activity/modules/webform.php
@@ -17,7 +17,15 @@ function campaignion_activity_webform_submission_insert($node, $submission) {
 }
 
 function campaignion_activity_email_confirmed($node, $submission) {
-  $activity = \Drupal\campaignion\Activity\WebformSubmission::bySubmission($node, $submission);
+  if (!($activity = \Drupal\campaignion\Activity\WebformSubmission::bySubmission($node, $submission))) {
+    watchdog('campaignion_activity', 'Trying to confirm a not yet logged submission: !nid, !sid', array('!nid' => $node->nid, '!sid' => $submission->sid), WATCHDOG_WARNING);
+    try {
+      $activity = \Drupal\campaignion\Activity\WebformSubmission::fromSubmission($node, $submission);
+    } catch (Exception $e) {
+      watchdog('campaignion_activity', 'Error when trying to log activity: !message', array('!message' => $e->getMessage()), WATCHDOG_WARNING);
+    }
+  }
   $activity->confirmed = time();
   $activity->save();
 }
+
