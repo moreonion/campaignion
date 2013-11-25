93ff4a518bdfe93fd711f6f8d6c42229a808c139
Roman Zimmermann <roman@more-onion.com>
2013-11-25 09:50:42 +0100

    Fixup ab translation-set folding. #3202

--- campaignion_manage/js/content_translations.js
+++ campaignion_manage/js/content_translations.js
@@ -1,12 +1,14 @@
 (function($) {
 
-$(document).ready(function() {
-  $('.campaignion-manage-content-listing .node-translations').hide().each(function() {
-    $translations = $(this);
-    $tset = $translations.prev();
+Drupal.behaviors.campaignion_manage_translation_sets = {};
+Drupal.behaviors.campaignion_manage_translation_sets.attach = function(context) {
+  $('.campaignion-manage-content-listing .node-translations', context).hide().each(function() {
+    var $translations = $(this);
+    var $tset = $translations.prev();
 
-    $showHideLink = $('<a class="show" href="#">' + Drupal.t('show translations') + '</a>').click(function(event) {
+    var $showHideLink = $('<a class="show" href="#">' + Drupal.t('show translations') + '</a>').click(function(event) {
       event.preventDefault();
+      var $showHideLink = $(this);
       if ($translations.is(':visible')) {
         $translations.hide();
         $showHideLink.html(Drupal.t('show translations')).addClass('show').removeClass('hide');
@@ -14,8 +16,8 @@ $(document).ready(function() {
         $translations.show();
         $showHideLink.html(Drupal.t('hide translations')).addClass('hide').removeClass('show');
       }
-    }).appendTo($tset.find('.content'));
+    }).appendTo($tset.find('.manage-content'));
   });
-});
+};
 
 })(jQuery);
