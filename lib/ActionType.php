8ccaedfee2e8507bc23a9faf2a27243af4be8d49
Roman Zimmermann <roman@more-onion.com>
2013-12-28 13:49:25 +0100

    action.module -> campaignion_action.module

--- lib/ActionType.php
+++ lib/ActionType.php
@@ -12,12 +12,12 @@ class ActionType {
   }
 
   public static function isAction($type) {
-    $action_types = \module_invoke_all('action_type_info');
+    $action_types = \module_invoke_all('campaignion_action_info');
     return isset($action_types[$type]);
   }
 
   public static function fromContentType($type) {
-    $action_types = \module_invoke_all('action_type_info');
+    $action_types = \module_invoke_all('campaignion_action_info');
     if (isset($action_types[$type])) {
       $class = $action_types[$type];
       return new $class();
