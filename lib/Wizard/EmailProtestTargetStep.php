81e9931493d0361119e975a4c013ab09ef36774a
Roman Zimmermann <roman@more-onion.com>
2014-01-16 13:24:37 +0100

    Remove dependency on mo_utilities.

--- lib/Wizard/EmailProtestTargetStep.php
+++ lib/Wizard/EmailProtestTargetStep.php
@@ -2,41 +2,35 @@
 
 namespace Drupal\campaignion\Wizard;
 
+use \Drupal\campaignion\Forms\EntityFieldForm;
+
 class EmailProtestTargetStep extends WizardStep {
 
   protected $step  = 'target';
   protected $title = 'Target';
 
+  protected $fieldForm = NULL;
+
   public function stepForm($form, &$form_state) {
 
     $form = parent::stepForm($form, $form_state);
 
-    module_load_include('inc', 'mo_utilities', 'mo_form');
-    $form += \MO\Form\get_entity_field_form('node', $this->wizard->node, array('field_protest_target_options', 'field_protest_target'), $form_state);
+    $this->fieldForm = new EntityFieldForm('node', $this->wizard->node, array('field_protest_target_options', 'field_protest_target'));
+    $form += $this->fieldForm->formArray($form_state);
 
     return $form;
   }
 
+  public function validateStep($form, &$form_state) {
+    $this->fieldForm->validate($form, $form_state);
+  }
+
   public function submitStep($form, &$form_state) {
-
-    $target_contacts = NULL;
-    foreach($form_state['values']['field_protest_target']['und'] as $key => $import_field) {
-      if ($key !== 'add_more' && isset($import_field['target_id'])) {
-        $target_contacts[(int) $import_field['target_id']] = redhen_contact_load((int) $import_field['target_id']);
-      }
-    }
-
-    $action_node_wrapper = entity_metadata_wrapper('node', $this->wizard->node);
-
-    $action_node_wrapper->field_protest_target->set($target_contacts);
-    $action_node_wrapper->field_protest_target_options->set($form_state['values']['field_protest_target_options']['und']['0']['value']);
-
-    $action_node_wrapper->save();
+    $this->fieldForm->submit($form, $form_state);
   }
 
   public function checkDependencies() {
     return isset($this->wizard->node->nid);
   }
-
 }
 
