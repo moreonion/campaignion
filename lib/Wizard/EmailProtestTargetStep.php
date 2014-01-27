27ec9b9c83ccfa3ef41cb4c96fef6121e2af734e
Roman Zimmermann <roman@more-onion.com>
2014-01-27 16:11:23 +0100

    Ticket #3395 : * fixes in webform component "protest_target" for form_builder * adjusted ae_webform_templates for new webform component "protest_target" * changed email protest wizard to use "protest_target" webform component

--- lib/Wizard/EmailProtestTargetStep.php
+++ lib/Wizard/EmailProtestTargetStep.php
@@ -17,64 +17,6 @@ class EmailProtestTargetStep extends WizardStep {
     return $form;
   }
 
-  protected function deleteComponent($form_key, &$form_state) {
-    foreach ($this->wizard->node->webform['components'] as $cid => $component) {
-
-      if ($component['form_key'] == $form_key) {
-        webform_component_delete($this->wizard->node, $component);
-        unset($this->wizard->node->webform['components'][$cid]);
-        break;
-      }
-    }
-  }
-
-  protected function insertProtestTargetComponent(&$target_contacts) {
-    $target_field_cid = NULL;
-    $fieldset_cid     = NULL;
-
-    foreach ($this->wizard->node->webform['components'] as $cid => &$component) {
-      if ($component['form_key'] == 'email_protest_target') {
-        $target_field_cid = $cid;
-        break;
-      }
-      elseif ($component['form_key'] == 'your_message') {
-        $fieldset_cid = $cid;
-      }
-    }
-
-    if ($target_field_cid == NULL) {
-      $_GET['name']      = 'Send your protest to';
-      $_GET['mandatory'] = 0;
-      $_GET['weight']    = -1;
-      $_GET['pid']       = $fieldset_cid;
-      $component         = webform_menu_component_load('new', $this->wizard->node->nid, 'select');
-
-      foreach(array('name', 'mandatory', 'weight', 'pid') as $i) {
-        unset($_GET[$i]);
-      }
-
-      $component['pid']                    = $fieldset_cid;
-      $component['form_key']               = 'email_protest_target';
-      $component['extra']['multiple']      = FALSE;
-      $component['extra']['aslist']        = TRUE;
-      $component['extra']['other_option']  = TRUE;
-      $component['extra']['title_display'] = 'before';
-      $component['extra']['custom_keys']   = FALSE;
-      $component['extra']['private']       = 0;
-
-      $target_field_cid = webform_component_insert($component);
-
-      $this->wizard->node->webform['components'][$target_field_cid] = $component;
-    }
-
-    $this->wizard->node->webform['components'][$target_field_cid]['extra']['items'] = '';
-    foreach ($target_contacts as $contact) {
-      $this->wizard->node->webform['components'][$target_field_cid]['extra']['items'] .= $contact->contact_id . "|" . $contact->first_name . " " . $contact->last_name . "\n";
-    }
-
-    webform_component_update($this->wizard->node->webform['components'][$target_field_cid]);
-  }
-
   public function submitStep($form, &$form_state) {
 
     $target_contacts = NULL;
@@ -90,17 +32,6 @@ class EmailProtestTargetStep extends WizardStep {
     $action_node_wrapper->field_protest_target_options->set($form_state['values']['field_protest_target_options']['und']['0']['value']);
 
     $action_node_wrapper->save();
-
-    if (isset($this->wizard->node->webform['components']) == TRUE) {
-      if ($form_state['values']['field_protest_target_options']['und']['0']['value'] == 'preselect') {
-        $this->deleteComponent('email_protest_target', $form_state);
-      }
-      else {
-        $this->insertProtestTargetComponent($target_contacts);
-      }
-    }
-
-    db_query('DELETE FROM form_builder_cache');
   }
 
   public function checkDependencies() {
