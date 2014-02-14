570d3ea5273c671d878993178ede2736505c17b1
Roman Zimmermann <roman@more-onion.com>
2014-02-14 17:08:39 +0100

    [cleverreach] ContactExporter. #3256

---
+++ campaignion_newsletters/campaignion_newsletters_cleverreach/lib/TagExporter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\campaignion_newsletters_cleverreach;
+
+use \Drupal\campaignion\CRM\Export\WrapperField;
+
+class TagExporter extends WrapperField {
+
+  public function value() {
+    $tags = $this->wrappedContact->{$this->key}->value();
+    $names = array();
+    foreach ($tags as $tag) {
+      $names[] = str_replace(',', '', $tag->name);
+    }
+    return ',' . implode(',', $names) . ',';
+  }
+}
