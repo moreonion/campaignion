26bce4a3a388f770472e4dd5af237f3de30dd3a6
Roman Zimmermann <roman@more-onion.com>
2014-02-20 11:02:44 +0100

    Migration for webform_paymethod_select.

--- campaignion_activity/modules/webform_paymethod_select.php
+++ campaignion_activity/modules/webform_paymethod_select.php
@@ -3,7 +3,7 @@
 /**
  * Implements hook_campaignion_action_info().
  */
-function webform_component_paymethod_select_campaignion_action_info() {
+function webform_paymethod_select_campaignion_action_info() {
   $info['webform_payment'] = 'Drupal\campaignion\Activity\WebformPayment';
 }
 
@@ -12,7 +12,7 @@ function webform_component_paymethod_select_campaignion_action_info() {
  */
 function campaignion_activity_payment_status_change(Payment $payment, PaymentStatusItem $previous_status_item) {
   $statusChangedToSuccess = $payment->getStatus()->status == PAYMENT_STATUS_SUCCESS && $previous_status_item->status != PAYMENT_STATUS_SUCCESS;
-  $hasContextObj = isset($payment->context_data['context']) && $payment->context_data['context'] instanceof \Drupal\webform_component_paymethod_select\WebformPaymentContext;
+  $hasContextObj = isset($payment->context_data['context']) && $payment->context_data['context'] instanceof \Drupal\webform_paymethod_select\WebformPaymentContext;
   if (!$statusChangedToSuccess || !$hasContextObj)
     return;
 
