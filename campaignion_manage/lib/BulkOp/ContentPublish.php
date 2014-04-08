2ce1415793700d9d6ff65487472909af114f0184
Roman Zimmermann <roman@more-onion.com>
2014-04-08 13:07:05 +0200

    [manage] Warnings for publish and unpublish bulk action.

--- campaignion_manage/lib/BulkOp/ContentPublish.php
+++ campaignion_manage/lib/BulkOp/ContentPublish.php
@@ -10,6 +10,13 @@ class ContentPublish {
     return t('Publishing your content will make it visible to the users of your site. Usually this includes all visitors of your site, but this depends on your permission settings.');
   }
   public function formElement(&$element, &$form_state) {
+    $element['warn']['#type'] = 'container';
+    $element['warn']['#attributes']['class'][] = 'warn';
+    $message = <<<STR
+You're about to publish !count pages. Publishing them will make them visible
+for almost all visitors of your website. Are you sure?
+STR;
+    $element['warn']['message']['#markup'] = t($message, array('!count' => '<span class="bulkop-count"></span>'));
   }
   public function apply($nids, &$form_state) {
     $nodes = node_load_multiple($nids);
