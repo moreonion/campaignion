a5e02cbb19b406e7a2fb36e1898815e564321d3a
Alex Berger <alex@more-onion.com>
2014-05-02 17:36:41 +0200

    recent supporters: timeago.js as library + locales
    - use timeago.js as library (using module libraries)
      (you have to provide the timeago library + locales in a libraries/
      directory)
    - define a variant per language
    - use the localized variant if we find one (using the current display
      language)

--- campaignion_recent_supporters/recent-supporters.js
+++ campaignion_recent_supporters/recent-supporters.js
@@ -1,31 +1,6 @@
 (function($) {
   Drupal.behaviors.recent_supporters = {};
   Drupal.behaviors.recent_supporters.attach = function (context, settings) {
-    // set locale after plugin is loaded and before the recent-supporters
-    // container is initialized
-    if ($.fn.timeago) {
-      // English (Template)
-      $.timeago.settings.strings = {
-        prefixAgo: "",
-        prefixFromNow: "",
-        suffixAgo: "ago",
-        suffixFromNow: "from now",
-        seconds: "%d seconds",
-        minute: "about a minute",
-        minutes: "%d minutes",
-        hour: "about an hour",
-        hours: "about %d hours",
-        day: "a day",
-        days: "%d days",
-        month: "about a month",
-        months: "%d months",
-        year: "about a year",
-        years: "%d years",
-        wordSeparator: " ",
-        numbers: []
-      };
-    }
-
     // start the recent-supporters polling
     if ($.fn.recentSupporters && settings.recentSupporters) {
       $('.block-campaignion-recent-supporters', context).recentSupporters({
