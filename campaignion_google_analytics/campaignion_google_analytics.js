bb078cab8c9be085aa4778624a14c07c9bb666d6
Paul Härle <paul@more-onion.com>
2014-08-25 06:11:29 +0200

    [google_analytics] track donations.

---
+++ campaignion_google_analytics/campaignion_google_analytics.js
@@ -0,0 +1,14 @@
+(function($) {
+    Drupal.behaviors.campaignion_google_analytics = {};
+    Drupal.behaviors.campaignion_google_analytics.attach = function(context, settings) {
+        var config =  settings.campaignion_google_analytics;
+        if (typeof config !== 'undefined' &&
+            typeof config.payment !== 'undefined') {
+            ga('require', 'ec');
+            ga('set', '&cu', config.payment.currency);
+            ga('ec:addProduct', config.payment);
+            ga('ec:setAction', 'purchase', {id: config.payment.id});
+            ga('send', 'event', 'Ecommerce', 'Purchase');
+        }
+    }
+})(jQuery);
