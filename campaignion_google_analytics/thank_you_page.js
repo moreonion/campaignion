a64d676396951f4e205a2748183ed96824220a0a
Paul Härle <paul@more-onion.com>
2014-08-26 18:51:55 +0200

    use older api, only track on thank you pages.

--- campaignion_google_analytics/thank_you_page.js
+++ campaignion_google_analytics/thank_you_page.js
@@ -2,15 +2,17 @@
     Drupal.behaviors.campaignion_google_analytics = {};
     Drupal.behaviors.campaignion_google_analytics.attach = function(context, settings) {
         var config =  settings.campaignion_google_analytics;
-        if (typeof config !== 'undefined' &&
-            typeof config.payment !== 'undefined') {
-            ga('require', 'ec');
-            ga('set', '&cu', config.payment.currency);
-            ga('ec:addProduct', config.payment);
-            ga('ec:setAction', 'purchase', {id: config.payment.id});
-            ga('send', 'event', 'Ecommerce', 'Purchase');
+        if (typeof config !== 'undefined') {
+            if (typeof config.thank_you !== "undefined") {
+                ga("send", "event", "webform", "submitted");
+            }
+            if (typeof config.payment !== 'undefined') {
+                ga('require', 'ecommerce');
+                ga('ecommerce:addTransaction', {id: config.payment.id});
+                ga('ecommerce:addItem', config.payment);
+                ga('ecommerce:send');
+            }
         }
-        ga('send', 'event', 'webform', 'submitted');
 
         $('.share-light li a').click(function(event) {
             ga('send', 'event', 'share', event.target.title);
