8c8d05ea82b6fc36ce184a221e28b3af444aaa0b
Roman Zimmermann <roman@more-onion.com>
2014-10-29 13:52:31 +0100

    Make the backend responsible for getting the right translation.

--- campaignion_recent_supporters/src/ActivityBackend.php
+++ campaignion_recent_supporters/src/ActivityBackend.php
@@ -20,7 +20,9 @@ class ActivityBackend {
     $config = $params->getParams();
     $addressfield = variable_get('campaignion_recent_supporters_address_field', 'field_address');
     $sql = <<<SQL
-SELECT rc.first_name, rc.last_name, ca.created as timestamp, wt.country, fdfa.{$addressfield}_country AS supporter_country, n.tnid, n.nid
+SELECT rc.first_name, rc.last_name, ca.created as timestamp,
+  COALESCE(fdfa.{$addressfield}_country, wt.country) AS country,
+  n.tnid, n.nid
 FROM {node} n
   INNER JOIN {campaignion_activity_webform} caw ON caw.nid = n.nid
   INNER JOIN {campaignion_activity} ca ON ca.activity_id=caw.activity_id AND ca.type='webform_submission'
@@ -33,7 +35,7 @@ ORDER BY ca.created DESC
 SQL;
     $result = db_query_range($sql, 0, $config['limit'], array(':nid' => $config['nid']));
 
-    return $this->buildSupporterList($result, $config['name_display'], FALSE);
+    return $this->buildSupporterList($result, $config['name_display']);
   }
 
   public function recentOnAllActions(RequestParams $params) {
@@ -43,23 +45,29 @@ SQL;
     $addressfield = variable_get('campaignion_recent_supporters_address_field', 'field_address');
     // get translations: na - activity node, nt - any available node translated into $lang, no - the "original" node (ie. the translation source)
     $sql = <<<SQL
-SELECT rc.first_name, rc.last_name, ca.created as timestamp, wt.country, fdfa.{$addressfield}_country AS supporter_country, na.tnid, na.type, na.title, na.nid, na.language, na.status, nt.language as tlanguage, nt.title as ttitle, nt.nid as nt_nid, nt.status as tstatus, no.language as olanguage, no.title as otitle, no.language as olanguage, no.nid as onid, no.status as ostatus
-  FROM {campaignion_activity_webform} caw
+SELECT rc.first_name, rc.last_name, ca.created as timestamp,
+  COALESCE(fdfa.{$addressfield}_country, wt.country) AS country,
+  na.nid, na.tnid, na.type AS action_type, na.status,
+  COALESCE(nt.title, no.title, na.title) AS action_title,
+  COALESCE(nt.tnid, no.tnid, na.nid) AS action_nid,
+  COALESCE(nt.language, no.language, na.language) AS action_lang
+FROM {campaignion_activity_webform} caw
   INNER JOIN {node} na ON caw.nid = na.nid
-  LEFT OUTER JOIN {node} nt ON na.tnid != 0 AND nt.tnid = na.tnid AND nt.language = :lang
-  LEFT OUTER JOIN {node} no ON na.tnid = no.nid
+  LEFT OUTER JOIN {node} nt ON na.tnid != 0 AND nt.tnid = na.tnid AND nt.language = :lang AND nt.status>0
+  LEFT OUTER JOIN {node} no ON na.tnid = no.nid AND no.status>0
   INNER JOIN {campaignion_activity} ca ON ca.activity_id = caw.activity_id AND ca.type = 'webform_submission'
   INNER JOIN {redhen_contact} rc USING (contact_id)
   INNER JOIN {webform_tracking} wt ON wt.nid = caw.nid AND wt.sid = caw.sid
   LEFT OUTER JOIN {field_data_$addressfield} fdfa ON fdfa.entity_id = rc.contact_id AND fdfa.delta = 0
-    WHERE na.status = 1 AND na.type IN (:types)
+WHERE na.status = 1 AND na.type IN (:types)
   ORDER BY ca.created DESC
 SQL;
     $result = db_query_range($sql, 0, $config['limit'], array(':types' => $config['types'], ':lang' => $config['lang']));
-    return $this->buildSupporterList($result, $config['name_display'], TRUE);
+    $rows = array();
+    return $this->buildSupporterList($result, $config['name_display']);
   }
 
-  function buildSupporterList($result, $name_display, $has_text = FALSE) {
+  function buildSupporterList($result, $name_display) {
     $supporters = array();
 
     // resolve "default" name display
@@ -75,12 +83,8 @@ SQL;
       $name_display = $name_display_default;
     }
 
-    global $base_url;
-
     foreach ($result as $item) {
-      $supporter = array();
-      $supporter['first_name'] = $item->first_name;
-      $supporter['last_name']  = $item->last_name;
+      $supporter = (array) $item;
 
       // no CAMPAIGNION_RECENT_SUPPORTERS_NAME_DISPLAY_DEFAULT any more in $name_display
       switch ($name_display) {
@@ -105,31 +109,9 @@ SQL;
           break;
       }
 
-      $supporter['timestamp']  = $item->timestamp;
       $supporter['rfc8601']    = date('c', $item->timestamp);
-      $supporter['country']    = empty($item->supporter_country) ? $item->country : $item->supporter_country;
-      if (isset($item->nid)) {
-        $supporter['nid'] = $item->nid;
-      }
-      if ($has_text) {
-        $supporter['nid']          = $item->nid;
-        $supporter['action_type']  = $item->type;
-
-        $supporter['action_title'] = $item->title;
-        $supporter['action_nid']   = $item->nid;
-        $supporter['action_lang']  = $item->language;
-        // overwrite action details with any translated informations
-        if (isset($item->ttitle) && $item->tstatus) {
-          $supporter['action_title'] = $item->ttitle;
-          $supporter['action_nid']   = $item->nt_nid;
-          $supporter['action_lang']  = $item->tlanguage;
-        } elseif (isset($item->otitle) && $item->ostatus) {
-          $supporter['action_title'] = $item->otitle;
-          $supporter['action_nid']   = $item->onid;
-          $supporter['action_lang']  = $item->olanguage;
-        }
-
-        $supporter['action_url']   = $base_url.'/node/'.$supporter['action_nid'];
+      if (isset($supporter['action_nid'])) {
+        $supporter['action_url']   = $GLOBALS['base_url'] . '/node/' . $supporter['action_nid'];
       }
       $supporters[] = $supporter;
     }
