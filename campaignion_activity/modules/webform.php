bc79b4d26016cb458635ba5b5031769dcb846ddf
Roman Zimmermann <roman@more-onion.com>
2015-01-14 19:35:30 +0100

    [activity] Don't log webform activities for drafts.

--- campaignion_activity/modules/webform.php
+++ campaignion_activity/modules/webform.php
@@ -5,14 +5,41 @@ function webform_campaignion_activity_type_info() {
 }
 
 /**
- * Implements hook_webform_submission_insert().
+ * Helper function to log a webform activity.
  */
-function campaignion_activity_webform_submission_insert($node, $submission) {
+function _campaignion_activity_webform_log($node, $submission) {
   try {
     $activity = \Drupal\campaignion\Activity\WebformSubmission::fromSubmission($node, $submission);
     $activity->save();
   } catch (Exception $e) {
     watchdog('campaignion_activity', 'Error when trying to log activity: !message', array('!message' => $e->getMessage()), WATCHDOG_WARNING);
+  }
+}
+
+/**
+ * Implements hook_webform_submission_insert().
+ */
+function campaignion_activity_webform_submission_insert($node, $submission) {
+  if ($submission->is_draft) {
+    return;
+  }
+  _campaignion_activity_webform_log($node, $submission);
+}
+
+/**
+ * Implements hook_webform_submission_update().
+ */
+function campaignion_activity_webform_submission_update($node, $submission) {
+  if ($submission->is_draft) {
+    return;
+  }
+  $was_draft = db_select('webform_submissions', 's')
+    ->fields('s', array('sid'))
+    ->condition('s.sid', $submission->sid)
+    ->execute()
+    ->fetchField();
+  if ($was_draft) {
+    _campaignion_activity_webform_log($node, $submission);
   }
 }
 
