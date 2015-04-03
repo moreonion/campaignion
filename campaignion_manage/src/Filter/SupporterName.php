49440ee5ed8fc092b0924f8f448fb868ea041f3d
Roman Zimmermann <roman@more-onion.com>
2015-04-03 19:26:35 +0200

    [manage] Search also searches in email addresses.

--- campaignion_manage/src/Filter/SupporterName.php
+++ campaignion_manage/src/Filter/SupporterName.php
@@ -18,11 +18,11 @@ class SupporterName extends Base implements FilterInterface {
 
   public function apply($query, array $values) {
     if (!empty($values['name'])) {
+      $alias = $query->join('field_data_redhen_contact_email', 'fdrce', "%alias.entity_type='redhen_contact' AND %alias.entity_id=r.contact_id");
       $search = preg_replace('/[[:blank:]]+/', '%', $values['name']);
       $search = preg_replace('/%%+/', '%', $search);
-      $search = preg_replace('/^%/', '', $search);
-      $search = preg_replace('/%$/', '', $search);
-      $query->where('LOWER(CONCAT(r.first_name, r.middle_name, r.last_name)) LIKE :search_string',
+      $search = trim($search, '%');
+      $query->where("LOWER(CONCAT(r.first_name, ' ', r.middle_name, ' ', r.last_name, ' ', $alias.redhen_contact_email_value)) LIKE :search_string",
         array(':search_string' => '%' . strtolower($search) . '%')
       );
     }
