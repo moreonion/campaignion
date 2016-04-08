0a87b9d2938df95fb92b8566c2fe53a5b91578a2
Roman Zimmermann <roman@more-onion.com>
2016-04-08 18:18:23 +0200

    Validate for a unique email field in most actions.

--- campaignion_wizard/src/PetitionWebformStep.php
+++ campaignion_wizard/src/PetitionWebformStep.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\campaignion_wizard;
 
-class PetitionWebformStep extends WebformStep {
+class PetitionWebformStep extends WebformStepUnique {
   public function status() {
     return array(
       'caption' => t('Your form is ready to go'),
