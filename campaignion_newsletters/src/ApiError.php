f70ccb99cbdff8deb85600d10eba483c018054cc
Roman Zimmermann <roman@more-onion.com>
2016-05-07 19:23:32 +0200

    Better messages for exceptions.

--- campaignion_newsletters/src/ApiError.php
+++ campaignion_newsletters/src/ApiError.php
@@ -8,13 +8,11 @@ class ApiError extends \Exception {
   public $link;
   public function __construct($backend, $message = '', $variables = array(), $code = 0, $link = NULL, \Exception $previous = NULL) {
     $this->backend = $backend;
-    $this->variables = $variables + array(
-      '@code' => $code,
-    );
+    $message = format_string($message, $variables + ['@code' => $code]);
     $this->link = $link;
     parent::__construct($message, $code, $previous);
   }
   public function log() {
-    \watchdog($this->backend, $this->message, $this->variables, WATCHDOG_ERROR, $this->link);
+    \watchdog($this->backend, $this->message, [], WATCHDOG_ERROR, $this->link);
   }
 }
