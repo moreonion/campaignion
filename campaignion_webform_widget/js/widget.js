c978ca226d5e545fd84094f6ccb08eb89ac997d8
Alex Berger <alex@more-onion.com>
2016-06-07 12:21:18 +0200

    [webform_widget] push _ga param to form action
    - we want to transfer the ga session to any newly opened pages too (when
      clicking on submit and a new tab opens)

--- campaignion_webform_widget/js/widget.js
+++ campaignion_webform_widget/js/widget.js
@@ -7,6 +7,47 @@ var messageParent = function(scrollTop){
     window.location.hash = 'h'+h;
   }
 }
+
+function parseQueryString(string) {
+  if (typeof string !== "string") {
+    string = "";
+  }
+  var vars = string.split('&');
+  var params = {};
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (!pair[0]) {
+      continue;
+    }
+    params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+  }
+  return params;
+}
+function serializeToQueryString(obj) {
+  var str = [];
+  for(var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  }
+  return str.join("&");
+}
+
+function gaLinkerHandler() {
+  var query = parseQueryString(document.location.search.substring(1));
+  var galinker = query['_ga'];
+
+  if (galinker) {
+    var form = document.getElementsByTagName("form")[0];
+    var url = document.createElement('a');
+    url.href = form.action;
+    var action = parseQueryString(url.search.substring(1))
+    action['_ga'] = galinker;
+    url.search = '?' + serializeToQueryString(action);
+    form.action = url.href;
+  }
+}
+
 window.onload = function() {
   messageParent();
 }
@@ -16,6 +57,7 @@ window.onresize = function() {
  
 if (window.addEventListener) {
   window.addEventListener("DOMSubtreeModified", function(){messageParent();}, true);
+  document.addEventListener("DOMContentLoaded", gaLinkerHandler);
 } else if (window.attachEvent) {
   window.attachEvent("onDOMSubtreeModified", function(){messageParent();});
 }
