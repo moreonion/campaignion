98d14dfc931ec7d1da0da02cc20f503c762402b7
Roman Zimmermann <roman@more-onion.com>
2016-07-01 16:52:08 +0200

    [crm:export:MappedWrapperField] Make it possible to pass through unmapped values.

--- src/CRM/Export/MappedWrapperField.php
+++ src/CRM/Export/MappedWrapperField.php
@@ -4,15 +4,21 @@ namespace Drupal\campaignion\CRM\Export;
 
 class MappedWrapperField extends WrapperField {
   protected $map;
-  public function __construct($key, $map) {
+  protected $filter;
+
+  public function __construct($key, $map, $filter_unmapped = TRUE) {
     parent::__construct($key);
     $this->map = $map;
+    $this->filter = $filter_unmapped;
   }
 
   public function value() {
     $value = parent::value();
     if (isset($this->map[$value])) {
       return $this->map[$value];
+    }
+    elseif (!$this->filter) {
+      return $value;
     }
   }
 }
