8c35e39589805519d71d6ce9cb105a7212b44118
Alex Berger <alex@more-onion.com>
2016-11-15 16:26:04 +0100

    pass embed param to TY page, render TY title
    - if a field named `field_pass_embed` (checkbox) is defined on an action
      and is toggle on, then pass the `embed` parameter to the redirected
      page
    - render the title if a TY page is displayed embedded

--- campaignion_webform_widget/templates/page--embedded.tpl.php
+++ campaignion_webform_widget/templates/page--embedded.tpl.php
@@ -1,4 +1,7 @@
 <?php if ($page['widget']): ?>
+  <?php if ($is_thankyou_page): ?>
+  <div id="headerwrap"><h1 class="title" id="page-title"><?php print $title; ?></h1></div>
+  <?php endif; ?>
   <div class="widget">
     <?php print render($page['widget']); ?>
   </div>
