b1b14b1b9219bc224abea53ef134fdd6a41a3cd7
Roman Zimmermann <roman@more-onion.com>
2017-03-12 07:27:33 +0100

    [email_protest] Copy over validation from field widget.

--- campaignion_wizard/src/EmailProtestTargetStep.php
+++ campaignion_wizard/src/EmailProtestTargetStep.php
@@ -23,6 +23,17 @@ class EmailProtestTargetStep extends WizardStep {
 
   public function validateStep($form, &$form_state) {
     $this->fieldForm->validate($form, $form_state);
+
+    $has_target = FALSE;
+    foreach ($form_state['values']['field_protest_target']['und'] as $delta => $values) {
+      if (is_numeric($delta) && !empty($form_state['values']['field_protest_target']['und'][$delta]['email_protest_target']['email'])) {
+        $has_target = TRUE;
+        break;
+      }
+    }
+    if ($has_target == FALSE) {
+      form_error($target['email'], t('You have to specify at least 1 target.'));
+    }
   }
 
   public function submitStep($form, &$form_state) {
