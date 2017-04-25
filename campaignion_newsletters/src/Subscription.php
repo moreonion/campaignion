cb9ed3df222bf938e65425127fa0beb764193390
Roman Zimmermann <roman@more-onion.com>
2017-04-25 16:06:41 +0200

    [newsletters] Add updated and last_sync fields to NewsletterList.

--- campaignion_newsletters/src/Subscription.php
+++ campaignion_newsletters/src/Subscription.php
@@ -6,6 +6,9 @@ class Subscription extends \Drupal\little_helpers\DB\Model {
   public $list_id;
   public $email;
   public $fingerprint = '';
+  public $updated = NULL;
+  public $last_sync = 0;
+
   public $delete = FALSE;
   public $source = NULL;
   public $needs_opt_in = FALSE;
@@ -16,7 +19,7 @@ class Subscription extends \Drupal\little_helpers\DB\Model {
 
   protected static $table = 'campaignion_newsletters_subscriptions';
   protected static $key = array('list_id', 'email');
-  protected static $values = array('fingerprint');
+  protected static $values = array('fingerprint', 'updated', 'last_sync');
   protected static $serial = FALSE;
 
   public static function byData($list_id, $email) {
@@ -58,12 +61,25 @@ class Subscription extends \Drupal\little_helpers\DB\Model {
     return self::$lists[$this->list_id];
   }
 
-  public function save($fromProvider = FALSE) {
+  /**
+   * Save the subscription to the database.
+   *
+   * @param bool $from_provider
+   *   TRUE if this is an update based on data from the provider and thus should
+   *   not be sent to provider.
+   * @param bool $update
+   *   TRUE if the updated timestamp should be updated.
+   */
+  public function save($from_provider = FALSE, $update = TRUE) {
+    if ($update) {
+      $this->updated = REQUEST_TIME;
+    }
+    $this->last_sync = max([$this->last_sync, $this->updated]);
     module_invoke_all('campaignion_newsletters_subscription_presave', $this);
     if ($this->delete) {
-      return $this->delete($fromProvider);
+      return $this->delete($from_provider);
     }
-    if (!$fromProvider && ($provider = $this->provider())) {
+    if (!$from_provider && ($provider = $this->provider())) {
       $item = QueueItem::byData(array(
         'list_id' => $this->list_id,
         'email' => $this->email,
