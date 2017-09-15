6231831fca2d342ce71e4f88f9eeabfa74b05a5e
Roman Zimmermann <roman@more-onion.com>
2017-09-15 14:33:50 +0200

    Activities for newsletter subscriptions
    - Implement new campaignion_newsletters hooks in campaignion_activity.
    - Add a new newsletter_subscription activity type (including model and
      code to track the activities).
    
    Revert "Revert feature"
    
    This reverts commit 9a7b420946ef8ffb4de7cc565f442cfbad71078c.

---
+++ campaignion_activity/modules/campaignion_newsletters.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Hook implementations for tracking newsletters subscription changes.
+ */
+
+use \Drupal\campaignion_activity\NewsletterSubscription;
+use \Drupal\campaignion_newsletters\Subscription;
+
+/**
+ * Implements campaignion_newsletters_subscription_saved().
+ */
+function campaignion_activity_campaignion_newsletters_subscription_saved(Subscription $subscription, $from_provider, $was_new) {
+  if ($was_new) {
+    NewsletterSubscription::fromSubscription($subscription, 'subscribe', $from_provider)->save();
+  }
+}
+
+/**
+ * Implements campaignion_newsletters_subscription_deleted().
+ */
+function campaignion_activity_campaignion_newsletters_subscription_deleted(Subscription $subscription, $from_provider) {
+  NewsletterSubscription::fromSubscription($subscription, 'unsubscribe', $from_provider)->save();
+}
