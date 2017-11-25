9fb66096a6d8f2907b1f11def738b9429fef873c
Onur D <contact@dinony.com>
2017-11-25 13:13:17 +0100

    Make lodash deps explicit. Set vue template compiler to the exact same version of vue in campaignion vue. Add bundle analyzer

--- campaignion_email_to_target/messages_app/build/webpack.prod.conf.js
+++ campaignion_email_to_target/messages_app/build/webpack.prod.conf.js
@@ -9,6 +9,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : config.build.env
@@ -51,7 +53,8 @@ var webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new BundleAnalyzerPlugin()
   ]
 })
 
