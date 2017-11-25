407742d6eecb2c399eadb3cf0bae45523edfc2ab
mayakokits <hello@mayakokits.com>
2017-11-25 11:20:41 +0100

    [wizard] Redirect app: add api server for e2e testing.

--- campaignion_wizard/redirects_app/src/utils/api.js
+++ campaignion_wizard/redirects_app/src/utils/api.js
@@ -1,15 +1,14 @@
 import axios from 'axios'
 
-// function paramReadyUrl (url) {
-//   if (!url.match(/\?[^=]+=[^&]*/)) {
-//     // there’s no parameter. replace trailing ? or / or /? with ?
-//     return url.replace(/[/?]$|(?:\/)\?$/, '') + '?'
-//   } else {
-//     // parameter present in the string. ensure trailing &
-//     return url.replace(/[&]$/, '') + '&'
-//   }
-// }
-//
+function paramReadyUrl (url) {
+  if (!url.match(/\?[^=]+=[^&]*/)) {
+    // there’s no parameter. replace trailing ? or / or /? with ?
+    return url.replace(/[/?]$|(?:\/)\?$/, '') + '?'
+  } else {
+    // parameter present in the string. ensure trailing &
+    return url.replace(/[&]$/, '') + '&'
+  }
+}
 
 export default {
   postData: function ({url, data, headers}) {
@@ -17,34 +16,7 @@ export default {
     return axios.put(url, data)
   },
   getNodes: function ({url, queryParam, queryString, headers}) {
-    // Mock server response:
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        var values = []
-        if (!queryString) {
-          for (let i = 0; i < 300; i++) {
-            values.push({
-              value: 'node/' + i,
-              label: 'Some Node title (' + i + ')'
-            })
-          }
-        } else {
-          for (let i = 0; i < 20 / queryString.length; i++) {
-            values.push({
-              value: 'node/' + i,
-              label: 'Some Node title containing ' + queryString + ' (' + i + ')'
-            })
-          }
-        }
-        resolve({
-          config: {
-            url: url + '?' + queryParam + '=' + queryString
-          },
-          data: {
-            values
-          }
-        })
-      }, 500)
-    })
+    // TODO authentification?
+    return axios.get(paramReadyUrl(url) + queryParam + '=' + queryString)
   }
 }
