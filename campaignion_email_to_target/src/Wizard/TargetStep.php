1a8fd6c131356eb74910663c7307a3921e819f5e
mayakokits <hello@mayakokits.com>
2017-11-26 11:44:45 +0100

    [e2t] Fields and settings for datasets app.

--- campaignion_email_to_target/src/Wizard/TargetStep.php
+++ campaignion_email_to_target/src/Wizard/TargetStep.php
@@ -3,6 +3,7 @@
 namespace Drupal\campaignion_email_to_target\Wizard;
 
 use \Drupal\campaignion\Forms\EntityFieldForm;
+use \Drupal\campaignion_email_to_target\Api\Client;
 
 class TargetStep extends \Drupal\campaignion_wizard\WizardStep {
   protected $step  = 'target';
@@ -13,6 +14,28 @@ class TargetStep extends \Drupal\campaignion_wizard\WizardStep {
     $field = $this->wizard->parameters['email_to_target']['options_field'];
     $this->fieldForm = new EntityFieldForm('node', $this->wizard->node, [$field]);
     $form += $this->fieldForm->formArray($form_state);
+
+    $settings = [];
+    $client = Client::fromConfig();
+    $token = $client->getAccessToken();
+    $settings['endpoints']['e2t-api'] = [
+      'url' => $client->getEndpoint(),
+      'token' => $token
+    ];
+
+    $settings = ['campaignion_email_to_target' => $settings];
+    $dir = drupal_get_path('module', 'campaignion_email_to_target');
+    $form['#attached']['js'][] = ['data' => $settings, 'type' => 'setting'];
+    $form['#attached']['js'][] = [
+      'data' => $dir . '/js/datasets_app/datasets_app.vue.min.js',
+      'scope' => 'footer',
+      'preprocess' => FALSE,
+    ];
+    $form['#attached']['css'][] = [
+      'data' => $dir . '/css/datasets_app/datasets_app.css',
+      'group' => 'CSS_DEFAULT',
+    ];
+
     return $form;
   }
 
