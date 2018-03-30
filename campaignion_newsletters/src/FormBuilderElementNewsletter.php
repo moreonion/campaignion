dad31ce2c7224f2bda2e340ab52dcd06bcedc5e3
Roman Zimmermann <roman@more-onion.com>
2018-03-30 11:10:20 +0200

    Improve newsletter component form UX.

--- campaignion_newsletters/src/FormBuilderElementNewsletter.php
+++ campaignion_newsletters/src/FormBuilderElementNewsletter.php
@@ -15,25 +15,26 @@ class FormBuilderElementNewsletter extends \FormBuilderWebformElement {
 
     $component = $this->element['#webform_component'];
 
-    // In order for the groups to work we need a flattened array of elements.
+    $form['#property_groups']['lists'] = [
+      'title' => t('Lists'),
+      'weight' => 2,
+    ];
+
+    // Only top-level elements can be assigned to property groups.
     // @see form_builder_field_configure_pre_render()
     $edit = _webform_edit_newsletter($component);
-    $form['options'] = array(
-      '#form_builder' => array('property_group' => 'options'),
-      '#tree' => TRUE,
-    );
-    $form['options']['lists'] = $edit['extra']['lists'];
-    $form['value'] = $edit['value'];
+    $form['lists'] = ['#type' => NULL] + $edit['list_management'];
+    $form['lists']['#form_builder']['property_group'] = 'lists';
+    $form['value'] = $edit['behavior']['value'];
     $form['description'] = $edit['extra']['description'];
-    $form['opt_in_implied'] = $edit['extra']['opt_in_implied'];
-    $form['send_welcome'] = $edit['extra']['send_welcome'];
     $dp['#form_builder']['property_group'] = 'display';
     $form['display'] = $edit['extra']['display'] + $dp;
     $form['checkbox_label'] = $edit['extra']['checkbox_label'] + $dp;
     $form['radio_labels'] = $edit['extra']['radio_labels'] + $dp;
     $form['optin_statement'] = $edit['extra']['optin_statement'];
-    $form['no_is_optout'] = $edit['extra']['no_is_optout'];
-    $form['optout_all_lists'] = $edit['extra']['optout_all_lists'];
+    $form['no_is_optout'] = $edit['behavior']['no_is_optout'];
+    $form['optout_all_lists'] = $edit['behavior']['optout_all_lists'];
+
     return $form;
   }
 
