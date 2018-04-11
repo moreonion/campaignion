36c743f125ee2acd0e74a5694141be332f78dae3
Roman Zimmermann <roman@more-onion.com>
2018-04-11 16:57:31 +0200

    Consistently use the same values accross the module.

--- campaignion_opt_in/campaignion_opt_in.conditionals.js
+++ campaignion_opt_in/campaignion_opt_in.conditionals.js
@@ -10,14 +10,14 @@
   Drupal.webform.conditionalOperatorOptInEqual = function (element, existingValue, ruleValue) {
     var checkbox = element.querySelector('.form-type-checkbox input');
     if (checkbox) {
-      return checkbox.checked ? ruleValue === 'yes' : ruleValue === '';
+      return checkbox.checked ? ruleValue === checkbox.value : ruleValue === '';
     }
     var radio = element.querySelector('.form-type-radio input:checked');
     if (radio) {
       // The radios use the same values as the conditional ruleValue.
       return element.querySelector('input:checked').value === ruleValue;
     }
-    // If no radio is selected at the moment this counts as "no opt-in".
+    // If no radio is selected at the moment this counts as "no change".
     return ruleValue === '';
   };
   Drupal.webform.conditionalOperatorOptInNotEqual = function (element, existingValue, ruleValue) {
---
+++ campaignion_opt_in/src/Values.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\campaignion_opt_in;
+
+/**
+ * Namespace for form-value constants.
+ */
+class Values {
+
+  const OPT_IN = 'opt-in';
+  const OPT_OUT = 'opt-out';
+  const NO_CHANGE = 'no change';
+  const NOT_SELECTED = '';
+
+}
