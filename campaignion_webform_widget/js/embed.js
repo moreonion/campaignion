3e1bbfa7b3f72f2dfcd9e0de85485bbf71cd79f5
Katharina Zwinger <katharina@more-onion.com>
2018-09-04 11:18:27 +0200

    Fix "matches is null" error
    String.prototype.match() returns `null` if there were no matches – `null` doesn’t have a length property.

--- campaignion_webform_widget/js/embed.js
+++ campaignion_webform_widget/js/embed.js
@@ -23,7 +23,7 @@
     var height, r,
       regex = new RegExp(xdomain + '$'),
       matches = e.origin.match(regex);
-    if(matches.length == 1){
+    if(matches){
       strD = e.data + "";
       r = strD.match(/^(\d+)(s?)$/);
       if(r && r.length == 3){
