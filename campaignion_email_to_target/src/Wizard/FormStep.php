fb4fcd86f54e8ff710b55d00f4ab8881b3ee6a1c
Roman Zimmermann <roman@more-onion.com>
2018-10-10 14:46:54 +0200

    e2t: Fix FormStep validation for flexible selectors.

--- campaignion_email_to_target/src/Wizard/FormStep.php
+++ campaignion_email_to_target/src/Wizard/FormStep.php
@@ -2,8 +2,9 @@
 
 namespace Drupal\campaignion_email_to_target\Wizard;
 
-use \Drupal\campaignion_action\Loader;
-use \Drupal\campaignion_wizard\WebformStepUnique;
+use Drupal\campaignion_action\Loader;
+use Drupal\campaignion_wizard\WebformStepUnique;
+use Drupal\little_helpers\Webform\Webform;
 
 /**
  * Extends the webform step with additional validations.
@@ -14,54 +15,36 @@ class FormStep extends WebformStepUnique {
    * Validate whether the resulting webform is something we can use.
    *
    * We need:
-   *   1. At least one postcode and one target selector field.
-   *   2. The postcode field must have the form_key postcode.
-   *   3. The target selector must be on a page after the (first) postcode
-   *      field.
+   *   1. At a webform component for each filter of the chosen selector.
+   *   2. The target selector must be on a page after the last filter component.
    */
   public function validateStep($form, &$form_state) {
     parent::validateStep($form, $form_state);
     // $form['#node'] has already been updated by form_builder_webform_save_form_validate().
-    $components = $form['#node']->webform['components'];
+    $dataset = Loader::instance()->actionFromNode($form['#node'])->dataset();
+    $webform = new Webform($form['#node']);
 
-    $custom = Loader::instance()->actionFromNode($form['#node'])->dataset()->isCustom;
-
-    $postcode = NULL;
-    $target_selector = NULL;
-    foreach ($components as $c) {
-      if ($c['type'] == 'postcode' && !$postcode) {
-        $postcode = $c;
+    $selector_pages = 1;
+    foreach ($dataset->selectors[0]['filters'] as $form_key => $metadata) {
+      if ($component = $webform->componentByKey($form_key)) {
+        $selector_pages = max($component['page_num'], $selector_pages);
       }
-      if ($c['type'] == 'e2t_selector' && !$target_selector) {
-        $target_selector = $c;
+      else {
+        form_error($form, t('Missing a form element for filtering targets: %title (with form key %form_key)', ['%title' => $metadata['title'], '%form_key' => $form_key]));
       }
     }
-    if ($custom) {
-      if (!$target_selector) {
-        form_error($form, t("The target selector field needs to be in the form. Please drag it in."));
+    $target_selector = NULL;
+    foreach ($webform->componentsByType('e2t_selector') as $c) {
+      $target_selector = $c;
+      break;
+    }
+    if ($target_selector) {
+      if ($target_selector['page_num'] <= $selector_pages) {
+        form_error($form, t('The target selector must appear on a page after all the target filters.'));
       }
     }
     else {
-      if ($postcode && $postcode['form_key'] != 'postcode') {
-        form_error($form, t('The postcode field has the wrong form key - it needs to be "postcode". Change it by clicking on the field, then click "edit" underneath Title field.'));
-      }
-      if ($postcode && $target_selector) {
-        if ($postcode['page_num'] == $target_selector['page_num']) {
-          form_error($form, t("The postcode and the target selector can't be placed on the same page. Add a 'page break' between the postcode field and the target selector field."));
-        }
-        elseif ($postcode['page_num'] > $target_selector['page_num']) {
-          form_error($form, t("The postcode field needs to be placed before the target selector field, with a page break in between."));
-        }
-      }
-      elseif (!$postcode && !$target_selector) {
-        form_error($form, t("The postcode field and the target selector field have to be available. Please add them to the form."));
-      }
-      elseif (!$postcode) {
-        form_error($form, t("The postcode field needs to be in the form. Please drag it in."));
-      }
-      elseif (!$target_selector) {
-        form_error($form, t("The target selector field needs to be in the form. Please drag it in."));
-      }
+      form_error($form, t("The target selector field needs to be in the form. Please drag it in."));
     }
   }
 
