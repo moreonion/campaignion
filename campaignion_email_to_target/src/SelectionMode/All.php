87d9db8f669211474a92123158a521c70108a513
Roman Zimmermann <roman@more-onion.com>
2018-11-10 16:31:41 +0100

    Refactor selection modes into a plugin system.

---
+++ campaignion_email_to_target/src/SelectionMode/All.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\campaignion_email_to_target\SelectionMode;
+
+/**
+ * Selection mode that requires that all messages are sent.
+ */
+class All extends Common {
+
+  /**
+   * Get the user readable title for this selectiom mode plugin.
+   *
+   * @return string
+   *   Translated title for this plugin.
+   */
+  public static function title() {
+    return t('Send email to all the targets.');
+  }
+
+  /**
+   * Get message edit form for a single target/message pair.
+   */
+  protected function messageForm($target, $message) {
+    $t = parent::messageForm($target, $message);
+    $t['#attributes']['class'][] = 'email-to-target-single';
+    $t['send'] = [
+      '#type' => 'markup',
+      '#markup' => "<p class=\"target\">{$message->display} </p>",
+      '#weight' => -1,
+    ];
+    return $t;
+  }
+
+  /**
+   * Get form element for choosing targets and previewing/editing messages.
+   */
+  public function formElement(array $pairs) {
+    $element = parent::formElement($pairs);
+    $element['#attached']['js'] = [drupal_get_path('module', 'campaignion_email_to_target') . '/js/target_selector.js'];
+    return $element;
+  }
+
+}
