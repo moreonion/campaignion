f3fd57d766bd42943a637b94d9c9189f93040fd6
mayakokits <hello@mayakokits.com>
2019-02-28 22:29:53 +0100

    [redirects_app] JSDoc comments for the redirects_app.

--- campaignion_wizard/redirects_app/src/store/state.js
+++ campaignion_wizard/redirects_app/src/store/state.js
@@ -1,10 +1,21 @@
 import {emptyRedirect} from '@/utils/defaults'
 
+/**
+ * State factory function.
+ * @return {Object} The initial vuex store state.
+ * {Object[]} createState().redirects - Collection of custom redirects.
+ * {Object} createState().defaultRedirect - The default redirect.
+ * {(integer|null)} createState().currentRedirectIndex - The index of the item in
+ *    the redirects array that is currently being edited or `-1` for a new item.
+ *    `null` means that no item is being edited.
+ * {Object} createState().initialData - The `defaultRedirect` and `redirects`
+ *    that were initially loaded.
+ */
 export function createState () {
   return {
     redirects: [],
     defaultRedirect: emptyRedirect(),
-    currentRedirectIndex: null, // the item in the redirects array that is currently edited or -1 for a new item
+    currentRedirectIndex: null,
     initialData: {
       redirects: [],
       defaultRedirect: {}
