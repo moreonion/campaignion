a873045cfc5421a9ef78fa8b75f6fb18b6487fac
Alex Berger <alex@more-onion.com>
2019-05-28 13:46:51 +0200

    campaignion_tracking: Basic event handling for GTM.

--- campaignion_tracking/package/src/gtm.js
+++ campaignion_tracking/package/src/gtm.js
@@ -1,34 +1,119 @@
-// eslint-disable-next-line camelcase
-let campaignion_tracking = window.campaignion_tracking
-// eslint-disable-next-line camelcase
-if (typeof campaignion_tracking === 'undefined') {
-  console.debug('No campaignion_tracking found. Doing nothing.')
-  return
-}
+/**
+ * Machine readable name.
+ *
+ * Use for referencing tracker specifics like storage keys, channel names, ...
+ */
+export const name = 'gtm'
 
-if (campaignion_tracking.debug) {
-  console.debug('campaignion_tracking_gtm')
-}
-
-let tracker = campaignion_tracking.tracker
-if (typeof tracker === 'undefined') {
-  console.debug('No TrackerManager found. Doing nothing.')
-  return
-}
-
+/**
+ * Mapping of codes to eventNames.
+ *
+ * This also is a whitelist of which events should *also* be recognized as
+ * codes.
+ */
 export const codes = {
-  cb: 'checkoutBegin',
-  ce: 'checkoutEnd',
   ds: 'donationSuccess'
 }
 
-let subscription = tracker.subscribe('gtm', (e) => {
-  console.debug('campaignion_tracking_gtm', 'handle_message', e)
-  let events = e.codes.reduce((acc, code) => {
-    if (codes[code]) {
-      acc.push(codes[code])
+/**
+ * Tracker for Google Tag Manager.
+ *
+ * Implements behaviour to dispatch events to GTM.
+ */
+export class GTMTracker {
+  /**
+   * Constructor.
+   *
+   * @param {TrackerManager} tracker the shared tracker manager object
+   * @param {Boolean} debug set to true for debugging
+   */
+  constructor (tracker, debug = false) {
+    this.debug = debug
+    this.tracker = tracker
+
+    this.printDebug('init')
+
+    if (typeof this.tracker === 'undefined') {
+      this.printDebug('No TrackerManager found. Doing nothing.')
     }
-    return acc
-  }, [])
-  console.debug('campaignion_tracking_gtm', 'handle_events', events)
-})
+
+    /**
+     * Subscribe to messages of the `donation` tracking channel.
+     *
+     * TODO: validate event, sanitize data
+     */
+    this.donationSubscription = this.tracker.subscribe('donation', (e) => {
+      this.printDebug('campaignion_tracking_gtm', 'handle_donation', e)
+
+      this.printDebug('campaignion_tracking_gtm', 'handle_event', e.name, e.data, e.context)
+
+      // dispatch to my handlers
+      this.dispatch(e.name, e.data, e.context)
+    })
+  }
+
+  /**
+   * Utility function to print to `console.debug`.
+   *
+   * Print only if debug is set to a truthy value.
+   *
+   * @param  {...any} args arguments to print
+   */
+  printDebug (...args) {
+    if (this.debug) {
+      console.debug('[campaignion_tracking]', '(gtm)', ...args)
+    }
+  }
+
+  /**
+   * Dispatch to my handlers.
+   *
+   * The handler are named according to the `eventName`, prefixed with
+   * `handle_`. The handlers are responsible for the actual sending of valid
+   * events to GTM.
+   *
+   * @param {String} eventName the name of the event
+   * @param {object} eventData data of the event
+   * @param {object} context context of the event, e.g. site title, ...
+   */
+  dispatch (eventName = '', eventData = {}, context = {}) {
+    if (typeof this['handle_' + eventName] === 'function') {
+      this['handle_' + eventName](eventName, eventData, context)
+    } else {
+      this.printDebug('no handler for event name:', eventName)
+    }
+  }
+
+  /**
+   * Handle "checkoutBegin".
+   *
+   * @param {String} eventName the event name
+   * @param {object} eventData data of the event
+   * @param {object} context context of the event
+   */
+  handle_checkoutBegin (eventName, eventData, context) { // eslint-disable-line camelcase
+    this.printDebug('(handle)', eventName, eventData, context)
+  }
+
+  /**
+   * Handle "checkoutEnd".
+   *
+   * @param {String} eventName the event name
+   * @param {object} eventData data of the event
+   * @param {object} context context of the event
+   */
+  handle_checkoutEnd (eventName, eventData, context) { // eslint-disable-line camelcase
+    this.printDebug('(handle)', eventName, eventData, context)
+  }
+
+  /**
+   * Handle "donationSuccess".
+   *
+   * @param {String} eventName the event name
+   * @param {object} eventData data of the event
+   * @param {object} context context of the event
+   */
+  handle_donationSuccess (eventName, eventData, context) { // eslint-disable-line camelcase
+    this.printDebug('(handle)', eventName, eventData, context)
+  }
+}
