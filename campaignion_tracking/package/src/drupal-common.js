94e207feb6a6fb4922358294495e40b3fb37ec18
Alex Berger <alex@more-onion.com>
2019-05-29 15:26:02 +0200

    campaignion_tracking: Parcel builds for Drupal.
    - campaignion_tracking_common.min.js and campaignion_tracking_gtm.min.js
      should be separate files
    - campaignion_tracking_gtm.min.js depends on
      campaignion_tracking_common.min.js being already loaded

---
+++ campaignion_tracking/package/src/drupal-common.js
@@ -0,0 +1,24 @@
+import * as tm from './tracker-manager'
+import * as listener from './listener'
+
+/**
+ * Check if we want debugging output.
+ *
+ * Parse the value as int, so we can disable debugging by setting to "0".
+ * `sessionStorage` only stores strings.
+ */
+// eslint-disable-next-line no-unneeded-ternary
+var debug = parseInt(sessionStorage.getItem('campaignion_debug')) ? true : false
+
+// some out-of-the-box prefixes
+// options: ['gtm']
+export const codePrefixes = []
+
+// common tracker manager, listener, gtm
+export const tracker = new tm.TrackerManager(debug)
+export const fragmentListener = new listener.FragmentListener(tracker, codePrefixes, debug)
+fragmentListener.setup()
+
+// re-exports
+export { tm, listener, debug }
+export { fragment } from './fragment'
