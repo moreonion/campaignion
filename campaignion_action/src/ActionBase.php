f5d00b0a9ff033135190600fa7ad61e52f16e2ee
Roman Zimmermann <roman@more-onion.com>
2020-05-04 12:21:11 +0200

    Move defaultTemplateNid() into ActionBase.

--- campaignion_action/src/ActionBase.php
+++ campaignion_action/src/ActionBase.php
@@ -31,7 +31,7 @@ class ActionBase {
       $node->webform_template = $node->translation_source->nid;
     } else {
       if (!isset($node->nid) && empty($node->webform['components'])) {
-        if ($nid = $this->type->defaultTemplateNid($node)) {
+        if ($nid = $this->defaultTemplateNid($node)) {
           // webform_template-7.x-1.x
           $_SESSION['webform_template'] = $nid;
           // webform_template-7.x-4.x
@@ -127,6 +127,30 @@ class ActionBase {
         }
         break;
     }
+  }
+
+  /**
+   * Get the default template node ID for the node.
+   *
+   * @param object $node
+   *   The node to get the template for.
+   *
+   * @return int|null
+   *   The node ID of the template node or NULL if no templates was found
+   */
+  public function defaultTemplateNid($node) {
+    $uuid = $this->type->parameters['template_node_uuid'];
+    $ids = \entity_get_id_by_uuid('node', [$uuid]);
+    if (($nid = array_shift($ids)) && ($template = node_load($nid))) {
+      if (module_exists('translation') && $template->tnid) {
+        $t = translation_node_get_translations($template->tnid);
+        if (isset($t[$node->language])) {
+          return $t[$node->language]->nid;
+        }
+      }
+      return $template->nid;
+    }
+    return NULL;
   }
 
 }
--- campaignion_action/src/ActionType.php
+++ campaignion_action/src/ActionType.php
@@ -22,30 +22,6 @@ class ActionType {
   }
 
   /**
-   * Get the default template node ID for the node.
-   *
-   * @param object $node
-   *   The node to get the template for.
-   *
-   * @return int|null
-   *   The node ID of the template node or NULL if no templates was found
-   */
-  public function defaultTemplateNid($node) {
-    $uuid = $this->parameters['template_node_uuid'];
-    $ids = \entity_get_id_by_uuid('node', [$uuid]);
-    if (($nid = array_shift($ids)) && ($template = node_load($nid))) {
-      if (module_exists('translation') && $template->tnid) {
-        $t = translation_node_get_translations($template->tnid);
-        if (isset($t[$node->language])) {
-          return $t[$node->language]->nid;
-        }
-      }
-      return $template->nid;
-    }
-    return NULL;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function isDonation() {
