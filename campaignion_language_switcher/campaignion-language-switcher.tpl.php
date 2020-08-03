223845d1ec1c4d1b89796158caa2b6b90fc720bc
Roman Zimmermann <roman@more-onion.com>
2020-08-03 11:43:29 +0200

    Only render accessible links by default.

--- campaignion_language_switcher/campaignion-language-switcher.tpl.php
+++ campaignion_language_switcher/campaignion-language-switcher.tpl.php
@@ -8,6 +8,7 @@
  * - $links: An array of $link-arrays. Each $link in $links contains:
  *   - $link['li_attributes']: Attribute array for the list-item.
  *   - $link['renderable']: A renderable array for the link content.
+ * - $links_accessible: Same as $links but all non-accessible links are removed.
  *
  * @see template_preprocess_campaignion_language_switcher()
  *
@@ -15,7 +16,7 @@
  */
 ?>
 <ul class="campaignion-language-switcher-<?php echo $provider; ?>">
-<?php foreach ($links as $link): ?>
+<?php foreach ($links_accessible as $link): ?>
   <li<?php echo drupal_attributes($link['li_attributes']); ?>><?php echo render($link['renderable']); ?></li>
 <?php endforeach; ?>
 </ul>
