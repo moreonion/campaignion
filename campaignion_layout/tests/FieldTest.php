4959a4fc9c1ce5b365c005e4657ac495ecbefe92
Roman Zimmermann <roman@more-onion.com>
2020-08-21 11:30:05 +0200

    [layout] Test for the field widget.

---
+++ campaignion_layout/tests/FieldTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\campaignion_layout;
+
+use Drupal\little_helpers\Services\Container;
+use Upal\DrupalUnitTestCase;
+
+/**
+ * Test for the field integration.
+ */
+class FieldTest extends DrupalUnitTestCase {
+
+  /**
+   * Clean up the injected services.
+   */
+  public function tearDown() {
+    Container::get()->inject('campaignion_layout.themes', NULL);
+    parent::tearDown();
+  }
+
+  /**
+   * Test rendering the field widget if no themes are available.
+   */
+  public function testFieldWidgetWithoutThemes() {
+    $themes = $this->createMock(Themes::class);
+    $themes->method('enabledThemes')->willReturn([]);
+    Container::get()->inject('campaignion_layout.themes', $themes);
+    $form = [];
+    $form_state = [];
+    $element = campaignion_layout_field_widget_form($form, $form_state, NULL, NULL, NULL, [], 0, []);
+    $this->assertEqual([], $element['theme']['#options']);
+    $this->assertFalse($element['#access']);
+  }
+
+  /**
+   * Test rendering the field widget with themes.
+   */
+  public function testFieldWidgetWithThemes() {
+    $theme_a = $this->createMock(Theme::class);
+    $theme_a->method('title')->willReturn('Theme A');
+    $theme_a->method('layouts')->willReturn([
+      '2col' => ['title' => 'Two columns', 'fields' => []],
+      'banner' => ['title' => 'Banner', 'fields' => ['banner' => TRUE]],
+    ]);
+    $theme_b = $this->createMock(Theme::class);
+    $theme_b->method('title')->willReturn('Theme B');
+    $theme_b->method('layouts')->willReturn([
+      '1col' => ['title' => 'Single column', 'fields' => []],
+    ]);
+    $themes = $this->createMock(Themes::class);
+    $themes->method('enabledThemes')->willReturn([
+      'a' => $theme_a,
+      'b' => $theme_b,
+    ]);
+    Container::get()->inject('campaignion_layout.themes', $themes);
+    $form = [];
+    $form_state = [];
+    $element = campaignion_layout_field_widget_form($form, $form_state, NULL, NULL, NULL, [], 0, []);
+    $this->assertEqual([
+      'a' => 'Theme A',
+      'b' => 'Theme B',
+    ], $element['theme']['#options']);
+    $this->assertNotEmpty($element['layout_a']['#options']);
+    $this->assertEqual([
+      '' => 'Default layout',
+      '2col' => 'Two columns',
+      'banner' => 'Banner',
+    ], $element['layout_a']['#options']);
+    $this->assertNotEmpty($element['layout_b']['#options']);
+    $this->assertEqual([
+      'banner' => ['#layout-a input' => ['banner']],
+    ], $form_state['campaignion_layout_fields']);
+
+    $element['theme']['#value'] = 'a';
+    $element['layout_a']['#value'] = 'banner';
+    $element['layout_b']['#value'] = '1col';
+    $element['layout']['#parents'] = ['layout'];
+    $form_state['values'] = [];
+    _campaignion_layout_field_widget_validate($element, $form_state, $form);
+    $this->assertEqual('banner', $form_state['values']['layout']);
+  }
+
+  /**
+   * Test field item empty check.
+   */
+  public function testIsEmpty() {
+    $item = ['theme' => '', 'layout' => 'foo'];
+    $this->assertTrue(campaignion_layout_field_is_empty($item, NULL));
+    $item = ['theme' => 'bar', 'layout' => 'foo'];
+    $this->assertFalse(campaignion_layout_field_is_empty($item, NULL));
+  }
+
+}
