339ad6112c34f3c9c3ed489870ff0262244d4534
Roman Zimmermann <roman@more-onion.com>
2022-03-24 12:51:31 +0100

    Completely rewrite as jQuery.validate validations

--- campaignion_loqate/package/src/main.js
+++ campaignion_loqate/package/src/main.js
@@ -2,19 +2,63 @@
 
 import regeneratorRuntime from 'regenerator-runtime'
 import { Client } from './client'
-import { Validator } from './validator'
+import * as validate from './validate'
 
-var $ = jQuery
-var client = null
+
+const $ = jQuery
+let client = null
 Drupal.behaviors.campaignion_loqate = {}
 Drupal.behaviors.campaignion_loqate.attach = function (context, settings) {
-  if (!settings.loqate) {
-    return
-  }
-  if (!client) {
+  if (settings.loqate && !client) {
     client = new Client(settings.loqate)
   }
-  $('.loqate-validate', context).each(function (index, wrapper) {
-    (new Validator(client, settings.loqate)).bind(wrapper).validate()
+  $(document).bind('clientsideValidationAddCustomRules', function (event) {
+    jQuery.validator.addMethod('loqateSortCode', function (value, element, param) {
+      const previous = this.previousValue(element)
+      if (previous.old === value) {
+        return previous.valid
+      }
+      previous.old = value
+      previous.message = this.defaultMessage(element, 'loqateSortCode')
+      this.startRequest(element)
+      client.validateSortCode(value).then((item) => {
+        const valid = !item.Error
+        this.stopRequest(element, valid)
+        previous.valid = valid
+        if (valid) {
+          validate.markValid.call(this, element)
+        }
+        else {
+          validate.markInvalid.call(this, element, previous.message)
+        }
+      })
+      return 'pending';
+    })
+    jQuery.validator.addMethod('loqateAccount', function (value, element, param) {
+      const $sortCodeElement = $(param.sortCodeElement)
+      if (!$sortCodeElement.is(':filled') || !$sortCodeElement.valid()) {
+        return 'dependency-mismatch';
+      }
+      const previous = this.previousValue(element)
+      if (previous.old === value) {
+        return previous.valid
+      }
+      previous.old = value
+      previous.message = this.defaultMessage(element, 'loqateAccount')
+      this.startRequest(element)
+      client.validateAccount($sortCodeElement.val(), value).then((item) => {
+        const valid = !item.Error
+        this.stopRequest(element, valid)
+        previous.valid = valid
+        if (valid) {
+          validate.markValid.call(this, element)
+        }
+        else {
+          validate.markInvalid.call(this, element, previous.message)
+        }
+      })
+      return 'pending';
+
+    })
   })
 }
